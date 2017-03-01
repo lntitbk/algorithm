bab4dd3f6bd73b73dde1dd669887aabbcae76f03
csit <trinh@trinh.vn>
2017-03-01 20:55:16 +0700

    merge sort

--- sort/insert_sort.cpp
+++ sort/insert_sort.cpp
@@ -34,17 +34,66 @@ void selection_sort(int* a, int array_size)
 	}
 }
 
-void merge_sort(int* a, int array_size)
+void merge(int* a, int low, int high, int mid)
 {
+	int MAX_SIZE = high - low + 1;
+	int* temp = new int[MAX_SIZE];
+	int i = low, j = mid + 1;
+	int k = 0;
 
+	while (i <= mid && j <= high)
+	{
+		if (a[i] > a[j])
+		{
+			temp[k] = a[j];
+			j++;
+		} 
+		else
+		{
+			temp[k] = a[i];
+			i++;
+		}
+		k++;
+	}
+
+	while (i <= mid)
+	{
+		temp[k] = a[i];
+		i++;
+		k++;
+	}
+
+	while (j <= high)
+	{
+		temp[k] = a[j];
+		j++;
+		k++;
+	}
+
+	for (int h = 0; h < MAX_SIZE; h++)
+	{
+		a[low + h] = temp[h];
+	}
+
+}
+
+void merge_sort(int* a, int low, int high)
+{
+	if (low < high)
+	{
+		int mid = (low + high)/2;
+		merge_sort(a, low, mid);
+		merge_sort(a, mid + 1, high);
+		merge(a,low,high,mid);	
+	}
 }
 
 int main(int argc, char* argv[])
 {
 	int arr[] = {1, 2, 4, 3};
-	int length = sizeof(arr)/sizeof(arr[0]);
-	insertion_sort(arr, length);
-	for (int i = 0; i < length; i++)
+	int array_size = sizeof(arr)/sizeof(arr[0]);
+	merge_sort(arr, 0, array_size - 1);
+	for (int i = 0; i < array_size; i++)
 	{
 		cout << arr[i] << "\n";
 	}
